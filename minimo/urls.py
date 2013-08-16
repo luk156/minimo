eb1c5696b83dfa12da259e22875afba4cbefed81
Scopel Emanuele <scopel.emanuele@silvergeko.it>
2013-08-16 18:56:01 +0200

    nuovo file system delle applicazioni -- aggiunti i file urls.py e __init__.py dimenticati nel commit

---
+++ minimo/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import patterns, include, url
+from dajaxice.core import dajaxice_autodiscover, dajaxice_config
+dajaxice_autodiscover()
+from django.conf import settings
+from django.conf.urls.static import static
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+
+
+urlpatterns = patterns('minimo',
+    # Examples:
+    url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
+    
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('minimo.fattura.urls')),
+) #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+
