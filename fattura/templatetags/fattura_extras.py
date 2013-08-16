8d820d868ddbd81d2bdd6bce18e04151efbdf07e
Scopel Emanuele <scopel.emanuele@silvergeko.it>
2013-08-16 18:41:52 +0200

    nuovo file system delle applicazioni

--- fattura/templatetags/fattura_extras.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def tot_imposta(instance,arg):
-	return instance.calcola(arg)
--- manage.py
+++ manage.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "minimo.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "minimo_test.settings")
 
     from django.core.management import execute_from_command_line
 
